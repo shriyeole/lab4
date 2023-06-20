dd4cac27aea05924c2298fe99bcfaee434198d43
shriyeole <shrijeetyeole2@gmail.com>
2023-06-20 20:01:16 +0530

    added

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+  standalone: true,
+  imports: [IonicModule, ExploreContainerComponent],
+})
+export class Tab3Page {
+  constructor() {}
+  isAlertOpen = false;
+  public alertButtons = ['OK'];
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
+}
